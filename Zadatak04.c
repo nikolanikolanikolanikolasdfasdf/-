523496b0b67a8ae646d180f482595ed1828a446a
nikolanikolanikolanikolasdfasdf <ognjenovic.skola@gmail.com>
2026-04-13 15:48:32 +0200

    Add files via upload

---
+++ Zadatak04.c
@@ -0,0 +1,43 @@
+﻿/*
+** ПРОЈЕКАТ : ::: Принципи програмирања :::
+**            Основе програмирања у програмском језику Ц
+** ДАТОТЕКА : Vezbe05 > Zadatak04 > Zadatak04.c
+** ОПИС     : Написати функцију која број прослеђен као параметар повећава за 2, али тако да промена буде видљива
+**			  и ван те функције.
+** ДАТУМ    : 02.02.2025.
+** ЛОКАЦИЈА : Београд
+** АУТОРИ   : Саша Д. Лазаревић, Татјана Стојановић, Кристина Јовановић
+** ПРОМЕНЕ  :
+**   <dd.mm.gggg.> - <опис промене> (<програмер>)
+**	 05.02.2025. - измењено заглавље програма (КЈ)
+**
+** Copyright (C) ФОН-ЛСИ/FON-LSI, 2025.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void povecaj_za_dva(int broj) {
+	broj = broj + 2;
+	printf("Unutar funkcije broj=%d\n", broj);
+}
+
+void povecaj_za_dva_pok(int* broj) {
+	*broj += 2;
+	printf("Unutar funkcije broj=%d\n", *broj);
+}
+
+int main() {
+
+	int broj = 5;
+
+	povecaj_za_dva(broj);
+	printf("Van funkcije broj=%d\n", broj);
+	// видимо да се вредност променљиве broj променила само унутар функције, али не и ван ње
+
+	povecaj_za_dva_pok(&broj);
+	printf("Van funkcije broj=%d\n", broj);
+	// видимо да се вредност променљиве broj променила и унутар и ван функције
+
+	return 0;
+}
---
+++ Zadatak05.c
@@ -0,0 +1,43 @@
+﻿/*
+** ПРОЈЕКАТ : ::: Принципи програмирања :::
+**            Основе програмирања у програмском језику Ц
+** ДАТОТЕКА : Vezbe05 > Zadatak05 > Zadatak05.c
+** ОПИС     : Написати функцију која враћа највећу цифру унетог броја.
+** ДАТУМ    : 02.02.2025.
+** ЛОКАЦИЈА : Београд
+** АУТОРИ   : Саша Д. Лазаревић, Татјана Стојановић, Кристина Јовановић
+** ПРОМЕНЕ  :
+**   <dd.mm.gggg.> - <опис промене> (<програмер>)
+**	 05.02.2025. - измењено заглавље програма (КЈ)
+**
+** Copyright (C) ФОН-ЛСИ/FON-LSI, 2025.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int najveca_cifra(int broj) {
+	int max = broj % 10;
+	broj = broj / 10;
+
+	while (1) {
+		int cifra_jedinica = broj % 10;
+		
+		if (cifra_jedinica > max)
+			max = cifra_jedinica;
+
+		broj = broj / 10;
+		if (broj == 0)		// услов за излазак из бесконачне петље
+			break;
+	}
+
+	return max;
+}
+
+int main() {
+
+	int broj = 4523;
+	printf("Najveca cifra u broju %d je %d.\n", broj, najveca_cifra(broj));
+
+	return 0;
+}
